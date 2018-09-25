48ea69ebb721946f5e89b46885a82ddc6b1e5861
Gaëtan FAVRAU <gfavrau@gmail.com>
2018-09-25 14:54:35 +0200

    création objet Annonce & AnnonceVM

---
+++ src/main/java/dev/domain/Annonce.java
@@ -0,0 +1,102 @@
+package dev.domain;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Annonce {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	private Collegue collegue;
+	private Vehicule vehicule;
+	private LocalDateTime horaireDeDepart;
+	private String lieuDeDepart;
+	private String lieuDeDestination;
+	private Integer nombreDeVoyageurs;
+	
+	public Annonce() {
+	}
+
+	public Annonce(Collegue collegue, Vehicule vehicule, LocalDateTime horaireDeDepart, String lieuDeDepart,
+			String lieuDeDestination, Integer nombreDeVoyageurs) {
+		this.collegue = collegue;
+		this.vehicule = vehicule;
+		this.horaireDeDepart = horaireDeDepart;
+		this.lieuDeDepart = lieuDeDepart;
+		this.lieuDeDestination = lieuDeDestination;
+		this.nombreDeVoyageurs = nombreDeVoyageurs;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Collegue getCollegue() {
+		return collegue;
+	}
+
+	public void setCollegue(Collegue collegue) {
+		this.collegue = collegue;
+	}
+
+	public Vehicule getVehicule() {
+		return vehicule;
+	}
+
+	public void setVehicule(Vehicule vehicule) {
+		this.vehicule = vehicule;
+	}
+
+	public LocalDateTime getHoraireDeDepart() {
+		return horaireDeDepart;
+	}
+
+	public void setHoraireDeDepart(LocalDateTime horaireDeDepart) {
+		this.horaireDeDepart = horaireDeDepart;
+	}
+
+	public String getLieuDeDepart() {
+		return lieuDeDepart;
+	}
+
+	public void setLieuDeDepart(String lieuDeDepart) {
+		this.lieuDeDepart = lieuDeDepart;
+	}
+
+	public String getLieuDeDestination() {
+		return lieuDeDestination;
+	}
+
+	public void setLieuDeDestination(String lieuDeDestination) {
+		this.lieuDeDestination = lieuDeDestination;
+	}
+
+	public Integer getNombreDeVoyageurs() {
+		return nombreDeVoyageurs;
+	}
+
+	public void setNombreDeVoyageurs(Integer nombreDeVoyageurs) {
+		this.nombreDeVoyageurs = nombreDeVoyageurs;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
