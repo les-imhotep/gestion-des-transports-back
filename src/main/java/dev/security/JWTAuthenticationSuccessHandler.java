d6c44951b390767011e604d89b6805a3062a0f14
Gaëtan FAVRAU <gfavrau@gmail.com>
2018-09-26 12:29:36 +0200

    modif auth

--- src/main/java/dev/security/JWTAuthenticationSuccessHandler.java
+++ src/main/java/dev/security/JWTAuthenticationSuccessHandler.java
@@ -72,13 +72,13 @@ public class JWTAuthenticationSuccessHandler extends SimpleUrlAuthenticationSucc
         String jws = Jwts.builder()
                 .setSubject(user.getUsername())
                 .addClaims(infosSupplementaireToken)
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRES_IN * 1000))
+                //.setExpiration(new Date(System.currentTimeMillis() + EXPIRES_IN * 1000))
                 .signWith(io.jsonwebtoken.SignatureAlgorithm.HS512, SECRET)
                 .compact();
 
         Cookie authCookie = new Cookie(TOKEN_COOKIE, (jws));
         authCookie.setHttpOnly(true);
-        authCookie.setMaxAge(EXPIRES_IN * 1000);
+       // authCookie.setMaxAge(EXPIRES_IN * 1000);
         authCookie.setPath("/");
         response.addCookie(authCookie);
         LOG.info("Token JWT généré posé dans un cookie et en entête HTTP");
